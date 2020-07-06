3d407a940f6d77030cb05d6003216065385079dc
XenonShawn <57314121+XenonShawn@users.noreply.github.com>
2020-07-06 19:37:23 +0800

    Redid the ORD counter

--- cog/nssg.py
+++ cog/nssg.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import pickle
 import logging
 from os.path import join
-from datetime import date
+from datetime import date as date_
 
 class nssg(commands.Cog):
 
@@ -25,48 +25,38 @@ class nssg(commands.Cog):
             pickle.dump(self.ord_dates, f, pickle.HIGHEST_PROTOCOL)
         logging.info("Saved ORD dates.")
 
-    @commands.command()
+    @commands.command(hidden=True)
     @commands.is_owner()
     async def check_nssg(self, ctx):
         """Owner only command for debugging purposes."""
         print(self.ord_dates)
 
     @commands.command()
-    async def ord(self, ctx, day: int, month: int, year: int):
+    async def ord(self, ctx, *date: int):
         """
-        Records your ORD.
+        Return how many days left to/since your ORD.
+        Use $ord [day] [month] [year] to set your ORD.
 
-        Usage: $ord [day] [month] [year]
+        Example: To set your ORD to 29/02/2020, use $ord 29 02 2020
+        Example: To check days to ORD, use $ord
         """
-        # Check for valid date
-        try:
-            self.ord_dates[ctx.author.id] = date(year, month, day)
-            return await ctx.send(f"Your ORD is set to {day:02d}/{month:02d}/{year}.")
-        except ValueError as e:
-            return await ctx.send(str(e).capitalize()) 
-
-    @commands.command()
-    async def countdown(self, ctx, user: discord.User=None):
-        """
-        Return the number of days left to ORD.
-
-        An optional user can be provided to check their number of days to ORD.
-        The user being check must have provided their ORD through $ord.
-        Usage: $countdown [optional: user]
-        """
-        user_id = user.id if user else ctx.author.id
-        if user_id not in self.ord_dates:
-            if user:
-                return await ctx.send("That user has not indicated their ORD.")
-            else:
+        if not len(date):
+            if ctx.author.id not in self.ord_dates:
                 return await ctx.send("You have not indicated your ORD. Use $ord [day] [month] [year] to set your ORD.")
-        days = (self.ord_dates[user_id] - date.today()).days
-        if days > 0:
-            return await ctx.send(f"{days} days left to ORD!")
-        elif days == 0:
-            return await ctx.send("WGT, ORDLO!")
+
+            days = (self.ord_dates[ctx.author.id] - date_.today()).days
+            if days > 0: await ctx.send(f"{days} days left to ORD!")
+            elif days == 0: await ctx.send("WGT, ORDLO!")
+            else: await ctx.send(f"{-days} days since ORD!")
+
+        elif len(date) == 3:
+            try:
+                self.ord_dates[ctx.author.id] = date_(date[2], date[1], date[0])
+                return await ctx.send(f"Your ORD is set to {date[0]:02d}/{date[1]:02d}/{date[2]}")
+            except ValueError as e:
+                return await ctx.send(str(e).capitalize())
         else:
-            return await ctx.send(f"{-days} days since ORD!")
+            await ctx.send("Inproper parameters to set your ORD. Use $ord [day] [month] [year] to set your ORD.")
         
 
 def setup(bot):
